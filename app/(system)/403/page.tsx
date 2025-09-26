5fa2123112be75c09b9c526d4170e58a19b0d66e
feitosa112 <112kuzmanovic@gmail.com>
2025-09-26 18:20:28 +0200

    System folder

---
+++ app/(system)/403/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import Link from "next/link";
+
+export default function Page403() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen bg-gray-50 text-gray-900 px-6">
+      System/403 page
+    </main>
+  );
+}
---
+++ app/(system)/404/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import Link from "next/link";
+
+export default function Page404() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen bg-gray-50 text-gray-900 px-6">
+      System/404 page
+    </main>
+  );
+}
