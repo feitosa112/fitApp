1811a378509784fa02b82d872d8cc1b994a1ae88
feitosa112 <112kuzmanovic@gmail.com>
2025-09-28 15:43:48 +0200

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-
-
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone', // ili ne koristi output: 'export'
+}
+module.exports = nextConfig;
