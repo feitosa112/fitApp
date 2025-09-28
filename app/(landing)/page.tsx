3d244a02dd2dd6c0ceaeaaf2c5ad3f2fd2dd68a6
feitosa112 <112kuzmanovic@gmail.com>
2025-09-28 16:41:33 +0200

    commit

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,6 +1,6 @@
 
 
-import Header from "./components/Header";
+
 import Hero from "./components/Hero";
 import Features from "./components/Features";
 import Testimonials from "./components/Testimonials";
@@ -12,7 +12,7 @@ import './styles/motion-classic.css';
 export default function LandingPage() {
   return (
     <div className="bg-light-vibrant text-light-vibrant-primary min-h-screen">
-      <Header />
+     
       <main className="pt-20">
         <Hero />
         <Features />
