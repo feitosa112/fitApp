7e0302b8ab09d0f5ec247d8e0c2883c04f3445af
feitosa112 <112kuzmanovic@gmail.com>
2025-09-28 15:30:06 +0200

    commit

---
+++ app/(system)/500/page.tsx
@@ -0,0 +1,8 @@
+export default function Custom500() {
+  return (
+    <main className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-gray-800">500 - Server Error</h1>
+      <p className="mt-2 text-gray-600">Something went wrong on our end.</p>
+    </main>
+  );
+}
