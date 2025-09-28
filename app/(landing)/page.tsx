f61ecebc55a118db2b72e842903f2bf368a66d05
feitosa112 <112kuzmanovic@gmail.com>
2025-09-28 15:56:07 +0200

    commit

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,25 +1,13 @@
-"use client"
-
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import Features from "./components/Features";
-import Testimonials from "./components/Testimonials";
-
-import './styles/motion-classic.css';
-
-export const dynamic = "force-dynamic";
+import Hero from './components/Hero';
+import Features from './components/Features';
+import Testimonials from './components/Testimonials';
 
 export default function LandingPage() {
   return (
-    <div className="bg-light-vibrant text-light-vibrant-primary min-h-screen">
-      <Header />
-      <main className="pt-20">
-        <Hero />
-        <Features />
-        <Testimonials />
-        
-      </main>
-      
-    </div>
+    <main>
+      <Hero />          {/* Hero je "use client" */}
+      <Features />      {/* Features je "use client" */}
+      <Testimonials />  {/* Testimonials je "use client" */}
+    </main>
   );
 }
